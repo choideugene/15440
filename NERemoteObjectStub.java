66b5a8c41733c1bbc9c0a3c68ccbc99a7f700cce
who7734 <choideugene@gmail.com>
2013-02-25 02:26:05 -0500

    updated NERemoteObjectStub

--- NERemoteObjectStub.java
+++ NERemoteObjectStub.java
@@ -6,10 +6,17 @@ public class NERemoteObjectStub implements NERemote {
   protected String hostAddress;
   protected int port;
   protected int key;
+  protected String remoteInterfaceName;
   
   public void init (NERemoteObjectReference objectRef) {
     hostAddress = objectRef.getHostAddress ();
     port = objectRef.getPort ();
     key = objectRef.getKey ();
+    remoteInterfaceName = objectRef.getRemoteInterfaceName ();
+  }
+  
+  public NERemoteObjectReference getRemoteObjectReference () {
+    return new NERemoteObjectReference 
+        (hostAddress, port, key, remoteInterfaceName);
   }
 }
